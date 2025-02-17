d5b98becd31a84a255ffbb88e71f4db52f0f35f4
Gurjinder <dev.gurjinder@gmail.com>
2025-02-17 15:35:12 +0000

    v1.0.0 commit

---
+++ src/index.js
@@ -0,0 +1,108 @@
+
+import { forwardRef, useState } from "react";
+
+const DragAndDrop = forwardRef(
+    (
+        {
+            setFileData = (e) => {
+                return e;
+            },
+            id = "uploadFile",
+            style = "",
+            size = "",
+            accept = ""
+        },
+        ref,
+    ) => {
+        const [file, setFile] = useState(null);
+        const [showWarning, setShowWarning] = useState(false);
+        const handleDrag = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+        };
+
+        const handleDrop = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (e.dataTransfer.files?.[0]) {
+                checkSize(e.dataTransfer.files[0]);
+            }
+        };
+
+        const uploadFile = (e) => {
+            if (e.target.files?.[0]) {
+                checkSize(e.target.files[0]);
+            }
+        };
+
+        const formatFileSize = (value) => {
+            if (value > 1000000) {
+                return Number(value / (1024 * 1024)).toFixed(2) + " MB";
+            }
+            return (Number(value / (1024 * 1024)) * 1000).toFixed(2) + " KB";
+        };
+
+        const removeFile = (e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            setFile(null);
+        };
+
+        const checkSize = (file) => {
+            setShowWarning(false);
+            console.log(file);
+
+            if (!!size) {
+                if (file.size > Number(size)*1000000) {
+                    setFile(null);
+                    setShowWarning(true);
+                } else {
+                    setFileData(file);
+                    setFile(file);
+                }
+            } else {
+                setFileData(file);
+                setFile(file);
+            }
+        
+        };
+
+        return (
+            
+            <label
+                onDragEnter={(e) => handleDrag(e)}
+                onDragLeave={(e) => handleDrag(e)}
+                onDragOver={(e) => handleDrag(e)}
+                onDrop={(e) => handleDrop(e)}
+                htmlFor={id}
+                className={style}
+            >
+                Choose Or drop a file
+                <input
+                    onChange={(e) => uploadFile(e)}
+                    id={id}
+                    ref={ref}
+                    type="file"
+                    placeholder=""
+                    accept={accept}
+                    className="inputfile"
+                />
+                {file && (
+                    <>
+                        <p>{file?.name}</p>
+                        <p>{formatFileSize(file?.size)}</p>
+                    </>
+                )}
+
+                {showWarning && (
+                    <p>File size limited to: {size} MB</p>
+                )}
+                <span onClick={(e) => removeFile(e)} title="Remove added file">
+                    &#128473;
+                </span>
+            </label>
+        );
+    },
+);
+
+export default DragAndDrop;
